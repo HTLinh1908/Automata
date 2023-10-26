53b37216d5ee5c69e82dd8da08c86069864ecbbd
HTLinh1908 <147149610+HTLinh1908@users.noreply.github.com>
2023-10-26 10:06:00 +0700

    Update RenameXML.py

--- RenameXML.py
+++ RenameXML.py
@@ -4,21 +4,20 @@ from bs4 import BeautifulSoup
 destination = str(input("Please enter the desired destination: "))
 
 XML_files = os.listdir(destination)
-
 for i in XML_files:
-    
+        
     if i[-3:]=='xml':
 
         with open(destination + '\\' + str(i), 'r', errors='ignore') as f:
             data = f.read()
-        
+                
             Bs_data = BeautifulSoup(data, "xml")
-        
-        
+                
             b_SHDon = Bs_data.find_all('SHDon')
-        
+
             SHDon = str(b_SHDon)
 
             SHDon= SHDon.lstrip('[<SHDon>').rstrip('</SHDon>]')
 
         os.rename(destination + '//' + str(i), destination + '//'+ SHDon + ".xml")
+
